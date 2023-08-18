1a72bb341cac1f0edc5ab2be4a0a45c912dacd88
Leo Geppert <lgeppert1145@stu.neisd.net>
2023-08-18 10:53:13 -0500

    Ms. Ghent trying to fix things

--- project.java
+++ project.java
@@ -6,13 +6,44 @@ public class project{
         System.out.println("What kind of math do you want?\n1 for add\n 2 for subtract\n3 for multiply\n4 for divide\n5 for modulate");
         int x = scan.nextInt();
         System.out.println("What numbers r u using? input 2 nubers");
-        int y = scan.nextInt();
-        int z = scan.nextInt();
+        double y = scan.nextDouble();
+        double z = scan.nextDouble();
         if(x == 1){
-            b.calcadd()
+            System.out.println("The answer to "+y+" plus "+z+" is "+b.calcadd(y,z));
         }
+        else if(x == 2){
+            System.out.println("The answer to "+y+" minus "+z+" is "+b.calcsub(y,z));
+        }
+        else if(x == 3){
+            System.out.println("The answer to "+y+" times "+z+" is "+b.calcmlt(y,z));
+        }
+        else if(x == 4){
+            System.out.println("The answer to "+y+" divided by "+z+" is "+b.calcdiv(y,z));
+        }
+        else if(x == 5){
+            System.out.println("The answer to "+y+" mod "+z+" is "+b.calcmod(y,z));
+        }
+        else{
+            System.out.println("That's not an option. Try again");
+        }
+        scan.close();
     }
 }
-public class calculator{
 
-}
+class calculator{
+    public double calcadd(double num1, double num2){
+        return num1 + num2;
+    }
+    public double calcsub(double num1, double num2){
+        return num1 - num2;
+    }
+    public double calcmlt(double num1, double num2){
+        return num1 * num2;
+    }
+    public double calcdiv(double num1, double num2){
+        return num1 / num2;
+    }
+    public double calcmod(double num1, double num2){
+        return num1 % num2;
+    }
+}//fin.
